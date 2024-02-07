7531f308df770c7f3f3850b78e5367c19e4c95de
nivethalsflearning <159346818+nivethalsflearning@users.noreply.github.com>
2024-02-07 16:03:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale , Spinach & Rocket smoothie')
+Streamlit.text('Hard-Boiled Free-Range Egg')
 
